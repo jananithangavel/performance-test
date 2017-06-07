5fb2da06b7437da3f8af16283e0cb46b95634035
Nirthika Rajendran <nirthika.rajendran@gmail.com>
2017-06-07 10:58:14 +0000

    Update start_ei.sh

--- EIInstance/start_ei.sh
+++ EIInstance/start_ei.sh
@@ -2,16 +2,25 @@
 export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_131
 
 if pgrep -f "carbon" > /dev/null; then
-    echo "Shutting down EI"
-    ~/ei/wso2ei-6.1.0/bin/integrator.sh stop
+echo "Shutting down EI"
+if [ $1 == "wso2ei-6.1.1" ] ; then
+    ~/product/$1/bin/integrator.sh stop
+else
+    ~/product/$1/bin/wso2server.sh stop
+fi
     sleep 30
 fi
 
-if [[ -f ~/ei/wso2ei-6.1.0/repository/logs/gc.log ]]; then
+if [[ -f ~/product/$1/repository/logs/gc.log ]]; then
     echo "GC Log exists. Moving to /tmp"
-    mv ~/ei/wso2ei-6.1.0/repository/logs/gc.log ~/tmp/
+    mv ~/product/$1/repository/logs/gc.log ~/tmp/
 fi
 
 echo "Starting EI"
-~/ei/wso2ei-6.1.0/bin/integrator.sh start
+if [ $1 == "wso2ei-6.1.1" ] ; then
+    ~/product/$1/bin/integrator.sh start
+else
+    ~/product/$1/bin/wso2server.sh start
+fi
+
 sleep 60
