4181e300b03c382f6784fd8d64d05e17ee5b9353
jananithangavel <janani22thangavel@gmail.com>
2017-06-23 11:05:12 +0530

    upadate variable issue

--- JmeterInstance/run_all.sh
+++ JmeterInstance/run_all.sh
@@ -15,8 +15,8 @@ fi
 
 concurrent_users=(10 50 100 500 1000 1500 2000 2500)
 proxy_type=(DirectProxy CBRProxy CBRSOAPHeaderProxy CBRTransportHeaderProxy SecureProxy XSLTEnhancedProxy XSLTProxy)
-request_payloads=(1K_buyStocks.xml 5K_buyStocks.xml 10K_buyStocks.xml 100K_buyStocks.xml 500K_buyStocks.xml 500B_buyStocks.xml)
-secure_payloads=(1K_buyStocks_secure.xml 5K_buyStocks_secure.xml 10K_buyStocks_secure.xml 100K_buyStocks_secure.xml 500K_buyStocks_secure.xml 500B_buyStocks_secure.xml)
+request_payloads=(1K_buyStocks.xml 5K_buyStocks.xml 10K_buyStocks.xml 100K_buyStocks.xml 500B_buyStocks.xml)
+secure_payloads=(1K_buyStocks_secure.xml 5K_buyStocks_secure.xml 10K_buyStocks_secure.xml 100K_buyStocks_secure.xml 500B_buyStocks_secure.xml)
 
 ei_host=192.168.48.172
 test_duration=600
@@ -28,7 +28,7 @@ run_tests() {
  for c in ${concurrent_users[@]} ; do
         for h in ${proxy_type[@]} ; do
                       mkdir -p ~/results/$c
-                      if [ h == "SecureProxy" ] ; then
+                      if [ $h == "SecureProxy" ] ; then
                            for i in ${secure_payloads[@]} ; do
                                ./esb-perf-execution.sh $ei_host $c $test_duration $h $i
                            done
